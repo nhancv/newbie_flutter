f300f191a02e6f9c0f0401380e38a9aa5811c738
Nhan Cao <nhancv92@gmail.com>
2019-11-11 16:35:05 +0700

    update app splash

--- android/app/src/main/kotlin/com/nhancv/newbie_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/nhancv/newbie_flutter/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.nhancv.newbie_flutter
-
+import android.os.Build
 import android.os.Bundle
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    //make transparent status bar
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      window.statusBarColor = 0x00000000
+    }
     GeneratedPluginRegistrant.registerWith(this)
+    //Remove full screen flag after load
+    val vto: ViewTreeObserver = flutterView.viewTreeObserver
+    vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+      override fun onGlobalLayout() {
+        flutterView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      }
+    })
   }
 }
